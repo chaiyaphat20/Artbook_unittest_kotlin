1993eec241f762c50e5e1ee820c94ff438fe1941
chaiyaphat supharak <chaiyaphat.b5922314@gmail.com>
2022-07-24 13:03:08 +0700

    feat: run

--- app/src/main/java/com/chaiyaphat/artbook_full_test/di/AppModule.kt
+++ app/src/main/java/com/chaiyaphat/artbook_full_test/di/AppModule.kt
@@ -2,20 +2,26 @@ package com.chaiyaphat.artbook_full_test.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.chaiyaphat.artbook_full_test.R
 import com.chaiyaphat.artbook_full_test.api.RetrofitAPI
+import com.chaiyaphat.artbook_full_test.repo.ArtRepository
+import com.chaiyaphat.artbook_full_test.repo.ArtRepositoryInterface
+import com.chaiyaphat.artbook_full_test.roomdb.ArtDao
 import com.chaiyaphat.artbook_full_test.roomdb.ArtDatabase
 import com.chaiyaphat.artbook_full_test.util.Util.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Singleton
@@ -31,10 +37,24 @@ object AppModule {
     @Singleton
     @Provides
     fun injectRetrofitAPI(): RetrofitAPI {
-        return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-            .create(RetrofitAPI::class.java)
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL).build().create(RetrofitAPI::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun injectNormalRepo(dao: ArtDao, api: RetrofitAPI) =
+        ArtRepository(dao, api) as ArtRepositoryInterface
+
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context) = Glide
+        .with(context).setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+        )
+
 
 }
--- app/src/main/java/com/chaiyaphat/artbook_full_test/view/MainActivity.kt
+++ app/src/main/java/com/chaiyaphat/artbook_full_test/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.chaiyaphat.artbook_full_test.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.chaiyaphat.artbook_full_test.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var fragmentFactory: ArtFragmentFactor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
     }
 }
